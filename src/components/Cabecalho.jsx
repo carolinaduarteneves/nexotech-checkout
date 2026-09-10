44a6f290ba521e0a9985e5807ebfa01f80e1dec4
Carolina Duarte Neves <carolinaduarteneves@gmail.com>
2026-09-10 10:40:06 -0300

    transforma o cabecalho em link de retorno ao carrinho

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -1,14 +1,17 @@
+import { Link } from 'react-router-dom'
 import logo from '../assets/img/logo.svg'
 
 export default function Cabecalho() {
   return (
     <header className="cabecalho">
       <div className="cabecalho__interno">
-        <img className="cabecalho__logo" src={logo} alt="" />
-        <div>
-          <p className="cabecalho__marca">NexoTech</p>
-          <p className="cabecalho__descricao">Checkout da loja</p>
-        </div>
+        <Link className="cabecalho__link" to="/">
+          <img className="cabecalho__logo" src={logo} alt="" />
+          <span>
+            <span className="cabecalho__marca">NexoTech</span>
+            <span className="cabecalho__descricao">Checkout da loja</span>
+          </span>
+        </Link>
       </div>
     </header>
   )
